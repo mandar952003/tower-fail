11826ee2c4924887fbcefe82689bf39ffccaf0b7
mandar952003 <77447919+mandar952003@users.noreply.github.com>
2021-05-22 15:58:00 +0530

    Add files via upload

---
+++ Tower-Siege-1-Template-main (1)/Tower-Siege-1-Template-main/slingshot.js
@@ -0,0 +1,12 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+     
+                                                                                        
+    }
+}
